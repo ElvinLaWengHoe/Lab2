748ea7b1926fc18480efdbbc5b43f4618ccfabd3
Elvin <ELVINLA.22@ichat.sp.edu.sg>
2023-10-23 22:53:54 +0800

    modified bmi and did exercise 2 to 4

--- bmi.py
+++ bmi.py
@@ -1,39 +1,17 @@
-'''def calculate_bmi(height, weight,bmi):
-    print("Height = " + height)
-    print("Weight = " + weight)
-    print("BMI = " + bmi)
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
 
-    bmi = weight/(height ** 2)
-    return bmi
+    bmi = weight/(height * height)
 
-
-
-weight = 57
-height = 1.73
-calculate_bmi(weight, height)'''
-
-
-def calculate_bmi(weight_kg, height_m):
-    # Check for invalid input
-    if weight_kg <= 0 or height_m <= 0:
-        return "Invalid input. Weight and height must be positive numbers."
-
-    # Calculate BMI
-    bmi = weight_kg / (height_m ** 2)
+    print("BMI = " + str(bmi))
 
     if bmi < 18.5:
-        print("You are Under Weight")
-    elif 18.5 <= bmi <= 25.0 :
-        print("You are Normal Weight")
-    else : print("You are Over Weight")
-    return bmi
+        print("Under Weight")
+    elif bmi < 25.0:
+        print("Normal Weight")
+    else:
+        print("Over Weight")
 
-
-
-
-# Example usage:
-weight = 60  # weight in kilograms
-height = 1.75  # height in meters
-bmi = calculate_bmi(weight, height)
-print(f"Your BMI is: {bmi:.2f}")
+calculate_bmi(weight=57, height=1.73)
 
